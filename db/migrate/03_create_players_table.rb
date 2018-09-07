d02214a322c834dfa45c428b4029889e74e3f5bc
Everett <everett.bowline@gmail.com>
2018-09-07 00:54:03 +0000

    Create Player elements

--- db/migrate/03_create_players_table.rb
+++ db/migrate/03_create_players_table.rb
@@ -1,3 +1,9 @@
 class CreatePlayersTable < ActiveRecord::Migration
-
+  def change
+   create_table :players do |t|
+     t.string :player_name
+     t.integer :age
+     t.string :player_country_of_origin
+   end
+  end
 end
