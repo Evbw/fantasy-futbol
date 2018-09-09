190da816af34d3a9806ab78004c3d0918639110d
Everett <everett.bowline@gmail.com>
2018-09-09 20:35:12 +0000

    Add user authentication to login post

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -16,5 +16,9 @@ class UserController < ApplicationController
 
   post '/login' do
     @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+    end
   end
+
 end
