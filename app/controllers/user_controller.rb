36c6a098f4fa4634bcf8ab01e7b4e059b6d5dd96
Everett <everett.bowline@gmail.com>
2018-09-11 02:14:30 +0000

    Update signup route to not allow blank values and redirect upon signup

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -9,9 +9,14 @@ class UserController < ApplicationController
   end
 
   post '/signup' do
-    @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
-    @user.save
-    session[:user_id] = @user.id
+    if params[:username] == '' || params[:password] == '' || params[:email] == ''
+      redirect to '/signup'
+    else
+      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect to '/teams'
+    end
   end
 
   get '/login' do
