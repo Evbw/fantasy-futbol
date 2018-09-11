150bc7b2d11f34f9909a1954fa6f498a30f16f16
Everett <everett.bowline@gmail.com>
2018-09-11 01:00:23 +0000

    Add post route to add new Players

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -9,4 +9,11 @@ class PlayerController < ApplicationController
     erb :'players/create_player'
   end
 
+  post '/players' do
+    @user = User.find_by(id: session[:user_id])
+    @player = Player.create(content: params[:content], user_id: @user.id)
+    @player.save
+    redirect to "/players/#{@player.id}"
+  end
+
 end
