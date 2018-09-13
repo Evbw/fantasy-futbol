f36f1147407c65c5371107008a14d1ab73dda4a6
Everett <everett.bowline@gmail.com>
2018-09-13 00:20:31 +0000

    Update routes to only work when a users is logged in

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -1,42 +1,56 @@
 class PlayerController < ApplicationController
 
   get '/players' do
-    @players = Player.all
-    erb :'players/players'
+    if logged_in?
+      @players = Player.all
+      erb :'players/players'
+    end
   end
 
   get '/players/new' do
-    erb :'players/create_player'
+    if logged_in?
+      erb :'players/create_player'
+    end
   end
 
   post '/players' do
-    @user = User.find_by(id: session[:user_id])
-    @player = Player.create(content: params[:content], user_id: @user.id)
-    @player.save
-    redirect to "/players/#{@player.id}"
+    if logged_in?
+      @user = User.find_by(id: session[:user_id])
+      @player = Player.create(content: params[:content], user_id: @user.id)
+      @player.save
+      redirect to "/players/#{@player.id}"
+    end
   end
 
   get '/players/:id' do
-    @player = Player.find_by_id(params[:id])
-    erb :'/players/show_player'
+    if logged_in?
+      @player = Player.find_by_id(params[:id])
+      erb :'/players/show_player'
+    end
   end
 
   get '/players/:id/edit' do
-    @player = Player.find_by_id(params[:id])
-    erb :'players/edit_player'
+    if logged_in?
+      @player = Player.find_by_id(params[:id])
+      erb :'players/edit_player'
+    end
   end
 
   patch '/players/:id' do
-    @player = Player.find(params[:id])
-    @player.update(player_name: params[:player_name])
-    @player.update(age: params[:age])
-    @player.update(player_country_of_origin: params[:player_country_of_origin])
-    redirect to "/players/#{@player.id}"
+    if logged_in?
+      @player = Player.find(params[:id])
+      @player.update(player_name: params[:player_name])
+      @player.update(age: params[:age])
+      @player.update(player_country_of_origin: params[:player_country_of_origin])
+      redirect to "/players/#{@player.id}"
+    end
   end
 
   delete '/players/:id/delete' do
-    @player.destroy
-    redirect to '/players'
+    if logged_in?
+      @player.destroy
+      redirect to '/players'
+    end
   end
 
 end
