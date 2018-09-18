ddbdacad7e00f62e5afaad7c70367113df1dc525
Everett <everett.bowline@gmail.com>
2018-09-18 00:33:40 +0000

    Add seeds file

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+t = Team.new
+t.team_name = "Tottenham Hotspurs"
+t.team_country = "England"
+
+pl = Player.new
+pl.player_name = "Harry Kane"
+pl.age = 25
+pl.player_country_of_origin = "England, probably"
