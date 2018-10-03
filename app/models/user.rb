ff5e0194e9f08836daee8f3832d6929189f1f69c
Everett <everett.bowline@gmail.com>
2018-10-03 01:21:09 +0000

    Add unique username validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   has_many :teams
   has_many :players, through: :teams
   has_secure_password
+  validates :username, uniqueness: true
+  validates :email, uniqueness: true
 end
