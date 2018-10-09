638bf24b62ec4d9a11189e8b41a72994a7274651
Everett <everett.bowline@gmail.com>
2018-10-09 13:07:58 +0000

    Refactor controller to use new login verification method

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -1,86 +1,67 @@
 class PlayerController < ApplicationController
 
   get '/players' do
-    if logged_in?
-      @players = Player.all
-      erb :'players/players'
-    else
-      redirect to '/login'
-    end
+    redirect_if_not_logged_in
+    @players = Player.all
+    erb :'players/players'
   end
 
   get '/players/new' do
-    if logged_in?
-      erb :'players/create_player'
-    else
-      redirect to '/login'
-    end
+    redirect_if_not_logged_in
+    erb :'players/create_player'
   end
 
   post '/players' do
-    if logged_in?
-      if params[:player_name] == "" || params[:age] == "" || params[:player_country_of_origin] == ""
-        redirect to "/players/new"
-      else
-        @user = User.find_by(id: session[:user_id])
-        @player = Player.create(player_name: params[:player_name], age: params[:age], player_country_of_origin: params[:player_country_of_origin], team_id: params[:user][:team_id])
-        @player.save
-        redirect to "/players/#{@player.id}"
-      end
+    redirect_if_not_logged_in
+    if params[:player_name] == "" || params[:age] == "" || params[:player_country_of_origin] == ""
+      redirect to "/players/new"
     else
-      redirect to '/login'
+      @user = User.find_by(id: session[:user_id])
+      @player = Player.create(player_name: params[:player_name], age: params[:age], player_country_of_origin: params[:player_country_of_origin], team_id: params[:user][:team_id])
+      @player.save
+      redirect to "/players/#{@player.id}"
     end
   end
 
   get '/players/:id' do
-    if logged_in?
-      @player = Player.find_by_id(params[:id])
-      erb :'/players/show_player'
-    else
-      redirect to '/login'
-    end
+    redirect_if_not_logged_in
+    @player = Player.find_by_id(params[:id])
+    erb :'/players/show_player'
   end
 
   get '/players/:id/edit' do
-    if logged_in?
-      @player = Player.find_by_id(params[:id])
-      if @player && @player.user == current_user
-        erb :'players/edit_player'
-      else
-        redirect to '/players'
-      end
+    redirect_if_not_logged_in
+    @player = Player.find_by_id(params[:id])
+    if @player && @player.user == current_user
+      erb :'players/edit_player'
     else
-      redirect to '/login'
+      redirect to '/players'
     end
   end
 
   patch '/players/:id' do
     @player = Player.find(params[:id])
     @user = User.find_by(id: session[:user_id])
-    if logged_in?
-      if !params[:player_name].empty? && !params[:player_country_of_origin].empty? && !params[:age].empty? && @team.user_id == @user.id
-        @player = Player.find(params[:id])
-        @player.update(player_name: params[:player_name])
-        @player.update(age: params[:age])
-        @player.update(player_country_of_origin: params[:player_country_of_origin])
-        redirect to "/players/#{@player.id}"
-      elsif params[:player_name].empty? && @player.user_id == @user.id
-        redirect to("/players/#{@player.id}/edit")
-      elsif !params[:player_name].empty? && @player.user_id != @user.id
-        redirect to("/players")
-      end
+    redirect_if_not_logged_in
+    if !params[:player_name].empty? && !params[:player_country_of_origin].empty? && !params[:age].empty? && @team.user_id == @user.id
+      @player = Player.find(params[:id])
+      @player.update(player_name: params[:player_name])
+      @player.update(age: params[:age])
+      @player.update(player_country_of_origin: params[:player_country_of_origin])
+      redirect to "/players/#{@player.id}"
+    elsif params[:player_name].empty? && @player.user_id == @user.id
+      redirect to("/players/#{@player.id}/edit")
+    elsif !params[:player_name].empty? && @player.user_id != @user.id
+      redirect to("/players")
     end
   end
 
   delete '/players/:id/delete' do
-    if logged_in?
-      @player = Player.find_by_id(params[:id])
-      if @player && @player.user == current_user
-        @player.destroy
-        redirect to '/players'
-      end
-    else
-      redirect to '/login'
+    redirect_if_not_logged_in
+    @player = Player.find_by_id(params[:id])
+    if @player && @player.user == current_user
+      @player.destroy
+      redirect to '/players'
     end
   end
 
